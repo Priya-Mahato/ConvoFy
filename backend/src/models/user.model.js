07e01c328ba5dde5662ecb420daec96cc264e5aa
Priya Mahato <priyamahato1950@gmail.com>
2025-06-22 00:04:45 +0530

    Changes done in user.model.js

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,6 +1,6 @@
 import mongoose, { Schema } from "mongoose";
 
-const userScheme = new Schema({
+const userSchema = new Schema({
   name: { type: String, required: true },
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
